e76a95a5cb30426aa9a929ae23821e93c4dd7839
Salman Qureshi <45896078+RundownCactus@users.noreply.github.com>
2022-01-15 06:26:41 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Portfolio';
+  githubStats: any;
+
+  constructor(private client: HttpClient) {
+    this.client
+      .get('https://api.github.com/users/salmanqureshi97')
+      .subscribe((data) => {
+        this.githubStats = data;
+      });
+  }
+
+  ngOnInit() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('profile-page');
+  }
+  ngOnDestroy() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('profile-page');
+  }
 }
